be513047e4582864fabc4d7e5f4a1a6de82fa3ef
RamiAlmuhana <ramealmuhana@hotmail.com>
2024-12-23 17:59:05 +0100

    Setting up database and first API test

--- Webshop/Controllers/UserController.cs
+++ Webshop/Controllers/UserController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Webshop.Data;
 using Webshop.Models;
 
 namespace Webshop.Controllers
@@ -8,20 +10,78 @@ namespace Webshop.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly DataContext _context;
+
+        public UserController(DataContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public async Task<ActionResult<List<User>>> GetAllUsers()
         {
-            var users = new List<User>
-            {
-                new User
-                {
-                    id = 1,
-                    email = "test@test.com",
-                    firstName = "test",
-                    lastName = "test"
-                }
-            };
+            var users = await _context.Users.ToListAsync();
+
             return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetSpecificUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound("User not found.");
+            }
+
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<User>>> AddUser(User user)
+        {
+            _context.Users.Add(user);
+
+            await _context.SaveChangesAsync();            
+
+            return Ok(await _context.Users.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<User>>> UpdateUsers(User updateUser)
+        {
+            var dbUser = await _context.Users.FindAsync(updateUser.id);
+
+            if (dbUser == null)
+            {
+                return NotFound("User not found.");
+            }
+
+            dbUser.email = updateUser.email;
+            dbUser.firstName = updateUser.firstName;
+            dbUser.lastName = updateUser.lastName;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Users.ToListAsync());
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<List<User>>> DeleteUsers(int id)
+        {
+            var dbUser = await _context.Users.FindAsync(id);
+
+            if (dbUser == null)
+            {
+                return NotFound("User not found.");
+            }
+
+            _context.Users.Remove(dbUser);
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Users.ToListAsync());
         }
     }
 }
