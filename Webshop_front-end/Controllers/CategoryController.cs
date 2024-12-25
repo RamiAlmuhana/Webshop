e4fcc553ab16be7a6f3b61496bd63f807ac72f47
RamiAlmuhana <ramealmuhana@hotmail.com>
2024-12-25 01:40:31 +0100

    New database and seed data in database
    Added a new table (Category) to the database and added data to the database for the category table

---
+++ Webshop_front-end/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Webshop_front_end.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Webshop_front-end/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Webshop_front_end.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
