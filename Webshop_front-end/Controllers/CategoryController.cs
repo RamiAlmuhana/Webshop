38e62c92ec5f8c1838a0199f444e1fc57e1ba5b9
RamiAlmuhana <ramealmuhana@hotmail.com>
2024-12-25 03:23:04 +0100

    Added bootswatch and bootstrap icons

--- Webshop_front-end/Controllers/CategoryController.cs
+++ Webshop_front-end/Controllers/CategoryController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Webshop_front_end.Data;
+using Webshop_front_end.Models;
 
 namespace Webshop_front_end.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
