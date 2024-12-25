3a528823b1c472fde4817b697dad26f20c7f64e7
RamiAlmuhana <ramealmuhana@hotmail.com>
2024-12-25 19:05:28 +0100

    You can create a category now and there is a client sided error

--- Webshop_front-end/Models/Category.cs
+++ Webshop_front-end/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Webshop_front_end.Models
 {
@@ -6,7 +7,11 @@ namespace Webshop_front_end.Models
     {
         public int Id { get; set; }
         [Required]
+        [DisplayName("Category Name")]
+        [MaxLength(30)]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "The field Display Order must be between 1-100.")]
         public int DisplayOrder { get; set; }
     }
 }
